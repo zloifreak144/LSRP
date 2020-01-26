773da615194e7da8eba3335d732715d9722b3a69
Jahangir <jahangir.gasimov@mail.mcgill.ca>
2020-01-26 15:19:18 -0500

    added "weight" field

--- src/main/java/socs/network/node/Link.java
+++ src/main/java/socs/network/node/Link.java
@@ -4,9 +4,11 @@ public class Link {
 
   RouterDescription router1;
   RouterDescription router2;
+  short weight;
 
-  public Link(RouterDescription r1, RouterDescription r2) {
+  public Link(RouterDescription r1, RouterDescription r2, short weight) {
     router1 = r1;
     router2 = r2;
+    this.weight = weight;
   }
 }
