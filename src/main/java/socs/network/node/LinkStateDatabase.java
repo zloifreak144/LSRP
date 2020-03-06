3c2b7b85bfba465506d2c02b24d49df64d399060
Jahangir <jahangir.gasimov@mail.mcgill.ca>
2020-03-06 17:46:22 -0500

    Shortest path

--- src/main/java/socs/network/node/LinkStateDatabase.java
+++ src/main/java/socs/network/node/LinkStateDatabase.java
@@ -57,6 +57,7 @@ public class LinkStateDatabase {
 
     if(!lsa.hasLink(ld))
     {
+      System.out.println("ADD");
       lsa.links.add(ld);
     }
   }
@@ -75,18 +76,124 @@ public class LinkStateDatabase {
     return sb.toString();
   }
 
-  public Path getShortestPath()
+  public String getShortestPath(String src, String dst)
   {
+    Path path = new Path();
 
+    Path result = findPath(src, dst, path);
+
+    return result == null ? "No path exists" : result.toString();
   }
+
+  //Recursive
+  private Path findPath(String src, String dst, Path path)
+  {
+    LSA current = _store.get(src);
+    LSA dest = _store.get(dst);
+    ArrayList<Path> paths = new ArrayList<>();
+
+    if(src.equals(dst))
+    {
+      return path;
+    }
+
+    short weight = Short.MAX_VALUE;
+
+    for(LinkDescription link : current.links)
+    {
+      if(path.contains(link.linkID) || current.linkStateID.equals(link.linkID))
+      {
+        continue;
+      }
+
+      if(path.getWeight() > weight)
+      {
+        path.removeFrom(current.linkStateID);
+      }
+      else
+      {
+        weight = path.getWeight();
+      }
+
+      path.addSegment(current.linkStateID, link.linkID, (short) link.tosMetrics);
+      Path temp = findPath(link.linkID, dst, new Path(path));
+
+      if(temp != null)
+      {
+        paths.add(temp);
+      }
+    }
+
+    return getSmallestPath(paths);
+  }
+
+  private Path getSmallestPath(ArrayList<Path> paths)
+  {
+    if(paths.isEmpty())
+    {
+      return null;
+    }
+    System.out.println("Size: " + paths.size());
+    Path smallestPath = paths.get(0);
+
+    for(Path path : paths)
+    {
+      if(smallestPath.getWeight() > path.getWeight())
+      {
+        smallestPath = path;
+      }
+    }
+
+    return smallestPath;
+  }
+
 
   class Path
   {
     ArrayList<PathSegment> path = new ArrayList<>();
 
+    Path()
+    {
+
+    }
+
+
+    Path(Path toCopy)
+    {
+      for(PathSegment segment : toCopy.path)
+      {
+        path.add(segment);
+      }
+    }
+
     void addSegment(String srcID, String destID, short weight)
     {
       path.add(new PathSegment(srcID, destID, weight));
+    }
+
+    boolean contains(String ID)
+    {
+      for(PathSegment segment : path)
+      {
+        if(ID.equals(segment.srcID) || ID.equals(segment.destID))
+        {
+          return true;
+        }
+      }
+
+      return false;
+    }
+
+    short getWeight()
+    {
+      short weight = 0;
+
+      for(PathSegment segment : path)
+      {
+        weight += segment.getWeight();
+      }
+
+      return path.isEmpty() ? Short.MAX_VALUE : weight;
     }
 
     void removeFrom(String srcID)
@@ -105,7 +212,7 @@ public class LinkStateDatabase {
 
       if(index != -1)
       {
-        for(int i = index; i < path.size(); i++)
+        for(int i = index + 1; i < path.size(); i++)
         {
           path.remove(i);
         }
@@ -145,8 +252,7 @@ public class LinkStateDatabase {
 
     @Override
     public String toString() {
-      return srcID + "-(" + weight + ")-" + destID;
+      return "[ " + srcID + "---(" + weight + ")---" + destID + " ]";
     }
   }
-
 }
